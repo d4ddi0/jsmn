3fa960c0a01b9e6bb1be4cfaba58a0d9a15ce2d6
Joshua Clayton <stillcompiling@gmail.com>
2018-11-03 23:20:52 -0600

    Refactor: p is a good name for parser
    Its short and not likely to cause confusion

--- js1.c
+++ js1.c
@@ -3,21 +3,21 @@
 /**
  * Fills token type and boundaries.
  */
-static int js1_fill_token(enum js1type type, struct js1_parser *parser,
+static int js1_fill_token(enum js1type type, struct js1_parser *p,
 			  int start, int end)
 {
-	struct js1token *token = parser->toknext++;
+	struct js1token *token = p->toknext++;
 
-	if (parser->toknext > parser->tokend)
+	if (p->toknext > p->tokend)
 		return JS1_ERROR_NOMEM;
 
 	token->type = type;
 	token->start = start;
 	token->end = end;
 	token->size = 0;
-	token->parent = parser->toksuper;
-	if (parser->toksuper)
-		parser->toksuper->size++;
+	token->parent = p->toksuper;
+	if (p->toksuper)
+		p->toksuper->size++;
 
 	return 0;
 }
@@ -25,44 +25,41 @@ static int js1_fill_token(enum js1type type, struct js1_parser *parser,
 /**
  * Fills next available token with JSON primitive.
  */
-static int js1_parse_primitive(struct js1_parser *parser, const char *js, size_t len)
+static int js1_parse_primitive(struct js1_parser *p, const char *js, size_t len)
 {
-	int next = parser->pos;
+	int next = p->pos;
 
 	for (; next < len && js[next] != '\0'; next++) {
 		switch (js[next]) {
 		case '\t' : case '\r' : case '\n' : case ' ' :
 		case ','  : case ']'  : case '}' :
-			goto found;
+			if (js1_fill_token(JS1_PRIMITIVE, p, p->pos, next))
+				return JS1_ERROR_NOMEM;
+
+			p->pos = next - 1;
+			return 0;
 		}
 		if (js[next] < 32 || js[next] >= 127) {
 			return JS1_ERROR_INVAL;
 		}
 	}
 	return JS1_ERROR_PART;
-
-found:
-	if (js1_fill_token(JS1_PRIMITIVE, parser, parser->pos, next))
-		return JS1_ERROR_NOMEM;
-	parser->pos = next;
-	parser->pos--;
-	return 0;
 }
 
 /**
  * Fills next token with JSON string.
  */
-static int js1_parse_string(struct js1_parser *parser, const char *js, size_t len)
+static int js1_parse_string(struct js1_parser *p, const char *js, size_t len)
 {
 	/* Skip starting quote */
-	for (int next = parser->pos + 1; next < len && js[next] != '\0'; next++) {
+	for (int next = p->pos + 1; next < len && js[next] != '\0'; next++) {
 		char c = js[next];
 
 		/* Quote: end of string */
 		if (c == '\"') {
-			if (js1_fill_token(JS1_STRING, parser, parser->pos+1, next))
+			if (js1_fill_token(JS1_STRING, p, p->pos+1, next))
 				return JS1_ERROR_NOMEM;
-			parser->pos = next;
+			p->pos = next;
 			return 0;
 		}
 
@@ -92,43 +89,43 @@ static int js1_parse_string(struct js1_parser *parser, const char *js, size_t le
 }
 
 /**
- * Parse JSON string and fill parser->tokens.
+ * Parse JSON string and fill tokens.
  */
-int js1_parse(struct js1_parser *parser, const char *js, size_t len)
+int js1_parse(struct js1_parser *p, const char *js, size_t len)
 {
 	int r;
-	if (!parser->tokens)
+	if (!p->tokens)
 		return JS1_ERROR_NOMEM;
 
-	for (; parser->pos < len && js[parser->pos] != '\0'; parser->pos++) {
+	for (; p->pos < len && js[p->pos] != '\0'; p->pos++) {
 		char c;
 		enum js1type type;
 		struct js1token *token;
 
-		c = js[parser->pos];
+		c = js[p->pos];
 		switch (c) {
 		case '{': case '[':
-			if (js1_fill_token((c == '{' ? JS1_OBJECT : JS1_ARRAY), parser, parser->pos, -1))
+			if (js1_fill_token((c == '{' ? JS1_OBJECT : JS1_ARRAY), p, p->pos, -1))
 				return JS1_ERROR_NOMEM;
-			parser->toksuper = parser->toknext - 1;
+			p->toksuper = p->toknext - 1;
 			break;
 		case '}': case ']':
 			type = (c == '}' ? JS1_OBJECT : JS1_ARRAY);
-			if (parser->toknext < parser->tokens + 1) {
+			if (p->toknext < p->tokens + 1) {
 				return JS1_ERROR_INVAL;
 			}
-			token = parser->toknext - 1;
+			token = p->toknext - 1;
 			for (;;) {
 				if (token->start != -1 && token->end == -1) {
 					if (token->type != type) {
 						return JS1_ERROR_INVAL;
 					}
-					token->end = parser->pos + 1;
-					parser->toksuper = token->parent;
+					token->end = p->pos + 1;
+					p->toksuper = token->parent;
 					break;
 				}
 				if (token->parent == NULL) {
-					if(token->type != type || !parser->toksuper) {
+					if(token->type != type || !p->toksuper) {
 						return JS1_ERROR_INVAL;
 					}
 					break;
@@ -137,33 +134,33 @@ int js1_parse(struct js1_parser *parser, const char *js, size_t len)
 			}
 			break;
 		case '\"':
-			r = js1_parse_string(parser, js, len);
+			r = js1_parse_string(p, js, len);
 			if (r < 0) return r;
 			break;
 		case '\t' : case '\r' : case '\n' : case ' ':
 			break;
 		case ':':
-			parser->toksuper = parser->toknext - 1;
+			p->toksuper = p->toknext - 1;
 			break;
 		case ',':
-			if (parser->toksuper &&
-			    parser->toksuper->type != JS1_ARRAY &&
-			    parser->toksuper->type != JS1_OBJECT) {
-				parser->toksuper = parser->toksuper->parent;
+			if (p->toksuper &&
+			    p->toksuper->type != JS1_ARRAY &&
+			    p->toksuper->type != JS1_OBJECT) {
+				p->toksuper = p->toksuper->parent;
 			}
 			break;
 		case '-': case '0': case '1' : case '2': case '3' : case '4':
 		case '5': case '6': case '7' : case '8': case '9':
 		case 't': case 'f': case 'n' :
 			/* And they must not be keys of the object */
-			if (parser->toksuper) {
-				struct js1token *t = parser->toksuper;
+			if (p->toksuper) {
+				struct js1token *t = p->toksuper;
 				if (t->type == JS1_OBJECT ||
 						(t->type == JS1_STRING && t->size != 0)) {
 					return JS1_ERROR_INVAL;
 				}
 			}
-			r = js1_parse_primitive(parser, js, len);
+			r = js1_parse_primitive(p, js, len);
 			if (r < 0) return r;
 			break;
 		default:
@@ -171,26 +168,26 @@ int js1_parse(struct js1_parser *parser, const char *js, size_t len)
 		}
 	}
 
-	for (struct js1token *token = parser->toknext - 1; token >= parser->tokens; token--) {
+	for (struct js1token *token = p->toknext - 1; token >= p->tokens; token--) {
 		/* Unmatched opened object or array */
 		if (token->start != -1 && token->end == -1) {
 			return JS1_ERROR_PART;
 		}
 	}
 
-	return parser->toknext - parser->tokens;
+	return p->toknext - p->tokens;
 }
 
 /**
- * Creates a new parser based over a given  buffer with an array of tokens
+ * Creates a new js1_parser based over a given  buffer with an array of tokens
  * available.
  */
-void js1_init(struct js1_parser *parser, struct js1token *tokens, size_t num_tokens)
+void js1_init(struct js1_parser *p, struct js1token *tokens, size_t num_tokens)
 {
-	parser->pos = 0;
-	parser->tokens = tokens;
-	parser->tokend = tokens + num_tokens;
-	parser->toknext = tokens;
-	parser->toksuper = NULL;
+	p->pos = 0;
+	p->tokens = tokens;
+	p->tokend = tokens + num_tokens;
+	p->toknext = tokens;
+	p->toksuper = NULL;
 }
 
